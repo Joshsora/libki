ac14fc5d743c7565c7a3da9485d9df2db35d53ef
Joshua Scott <joshsoragaming@gmail.com>
2018-04-02 01:25:08 +0100

    protocol: Definitions for control structures

---
+++ include/ki/protocol/control/Opcode.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdint>
+
+namespace ki
+{
+namespace protocol
+{
+namespace control
+{
+	enum class Opcode : uint8_t
+	{
+		SERVER_HELLO = 0,
+		UDP_HELLO = 1,
+		PING = 3,
+		PING_RSP = 4,
+		CLIENT_HELLO = 5
+	};
+}
+}
+}
